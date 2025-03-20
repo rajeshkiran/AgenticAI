ef23860ff5ee8572efaa4a9fd82e9c59969bef2d
rajeshkiran <42511333+rajeshkiran@users.noreply.github.com>
2025-03-20 19:42:14 +0530

    Add files via upload

---
+++ NeuraGuide/NeuraGuide/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NeuraGuide;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
